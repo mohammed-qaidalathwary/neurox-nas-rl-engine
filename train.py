20bfa4cdcdb8e70a7caabdddc43d1b8375295770
Mohammed Qaid Al-Athwary <mohammedqaidalathwary@gmail.com>
2026-05-12 00:19:08 +0300

    Initialize NeuroX with configuration settings

---
+++ train.py
@@ -0,0 +1,10 @@
+from config import Config
+
+def main():
+
+    print("NeuroX initialized")
+    print("Dataset:", Config.dataset)
+    print("Device:", Config.device)
+
+if __name__ == "__main__":
+    main()
